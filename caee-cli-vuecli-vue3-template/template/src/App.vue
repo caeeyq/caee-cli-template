81a2e6bf8176479fcdd301883f5b8597f897f603
yujie <158392613@qq.com>
2021-11-02 16:45:50 +0800

    init template project

---
+++ caee-cli-vuecli-vue3-template/template/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+  <router-view />
+</template>
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
