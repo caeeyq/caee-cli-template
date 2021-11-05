33e2176322e82e55595a0d3f00efd62dc848021c
yujie <158392613@qq.com>
2021-11-05 10:42:25 +0800

    feat: 添加caee-cli-vite-vue3-custom-template自定义模板

---
+++ templates/caee-cli-vite-vue3-custom-template/index.js
@@ -0,0 +1,12 @@
+const fse = require('fs-extra')
+
+function install(options) {
+  console.log('自定义模板接收信息=>', options)
+  const { targetPath, sourcePath } = options
+  fse.ensureDirSync(sourcePath)
+  fse.ensureDirSync(targetPath)
+  fse.copySync(templatePath, targetPath)
+  console.log('模板拷贝完成...')
+}
+
+module.exports = install
